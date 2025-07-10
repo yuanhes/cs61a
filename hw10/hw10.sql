93fca82f68cdd3190eaa9b191165c02eda1247b1
Franz Song <justsyh@gmail.com>
2025-07-11 02:00:14 +0800

    complete hw10

--- hw10/hw10.sql
+++ hw10/hw10.sql
@@ -26,24 +26,43 @@ CREATE TABLE sizes AS
 
 -- All dogs with parents ordered by decreasing height of their parent
 CREATE TABLE by_parent_height AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  -- SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.name
+    FROM dogs AS a, dogs AS b, parents
+    WHERE a.name = parents.child and b.name = parents.parent
+    ORDER BY b.height DESC
+;
 
 
 -- The size of each dog
 CREATE TABLE size_of_dogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  -- SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT dogs.name AS name, sizes.size AS size
+    FROM dogs, sizes
+    WHERE dogs.height > sizes.min and dogs.height <= sizes.max
+;
 
 -- Filling out this helper table is optional
 CREATE TABLE siblings AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  -- SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.child AS aname, b.child AS bname, sizea.size AS size
+    FROM parents AS a, parents AS b, size_of_dogs AS sizea, size_of_dogs AS sizeb
+    WHERE a.child = sizea.name and b.child = sizeb.name and a.parent = b.parent and a.child < b.child and sizea.size = sizeb.size
+;
 
 -- Sentences about siblings that are the same size
 CREATE TABLE sentences AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  --SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT CONCAT("The two siblings, ", s.aname, " and ", s.bname, ", have the same size: ", s.size)
+    FROM siblings AS s
+;
 
 -- Height range for each fur type where all of the heights differ by no more than 30% from the average height
 CREATE TABLE low_variance AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  --SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT fur, MAX(height) - MIN(height)
+    FROM dogs
+    GROUP BY fur
+    HAVING MAX(height)/AVG(height) < 1.3 AND MIN(height)/AVG(height) > 0.7
+;
 
