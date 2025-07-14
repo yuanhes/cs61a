522f27ae3501a25ab5dae4d067c41794a09bb555
Franz Song <justsyh@gmail.com>
2025-07-15 07:29:44 +0800

    complete project scheme interpreter part 2

--- scheme/scheme_forms.py
+++ scheme/scheme_forms.py
@@ -44,6 +44,12 @@ def do_define_form(expressions, env):
         # defining a named procedure e.g. (define (f x y) (+ x y))
         # BEGIN PROBLEM 10
         "*** YOUR CODE HERE ***"
+        symbol = signature.first
+        formals = signature.rest
+        validate_formals(formals)
+        procedure = LambdaProcedure(formals, expressions.rest, env)
+        env.define(symbol, procedure)
+        return symbol
         # END PROBLEM 10
     else:
         bad_signature = signature.first if isinstance(signature, Pair) else signature
@@ -224,6 +230,7 @@ def do_mu_form(expressions, env):
     validate_formals(formals)
     # BEGIN PROBLEM 11
     "*** YOUR CODE HERE ***"
+    return MuProcedure(formals, expressions.rest)
     # END PROBLEM 11
 
 
