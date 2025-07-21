f12a6d7e13e828c8acc97f515c29a739d12b2408
Franz Song <justsyh@gmail.com>
2025-07-21 22:12:17 +0800

    complete project scheme interpreter part 4

--- scheme/questions.scm
+++ scheme/questions.scm
@@ -7,7 +7,11 @@
 ;; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 15
-  'replace-this-line
+  (define (helper m rest)
+    (if (null? rest)
+        '()
+        (cons `(,m ,(car rest)) (helper (+ m 1) (cdr rest)))))
+  (helper 0 s)
   )
   ; END PROBLEM 15
 
@@ -17,7 +21,11 @@
 ;; the merged lists.
 (define (merge ordered? s1 s2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
+  (cond ((null? s1) s2)
+        ((null? s2) s1)
+        ((ordered? (car s1) (car s2)) 
+         (cons (car s1) (merge ordered? (cdr s1) s2)))
+        (else (cons (car s2) (merge ordered? s1 (cdr s2)))))
   )
   ; END PROBLEM 16
 
