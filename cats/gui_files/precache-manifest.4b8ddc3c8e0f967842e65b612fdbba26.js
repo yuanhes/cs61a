4b0650baccd955b5c62dd0581ce378713e62286d
Franz Song <justsyh@gmail.com>
2025-08-10 07:26:34 +0800

    include previously ignored gui_files

---
+++ cats/gui_files/precache-manifest.4b8ddc3c8e0f967842e65b612fdbba26.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565d2c26d758689050201228a00572fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2901c28304908cb4707",
+    "url": "/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "19164c1dc299c6a97128",
+    "url": "/static/css/main.57bd4654.chunk.css"
+  },
+  {
+    "revision": "c2901c28304908cb4707",
+    "url": "/static/js/2.5ef229f9.chunk.js"
+  },
+  {
+    "revision": "19164c1dc299c6a97128",
+    "url": "/static/js/main.da4d84a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "10bc42cc1eb0a90bdd49f50e614bce43",
+    "url": "/static/media/BlackCatFacingCenter.10bc42cc.png"
+  },
+  {
+    "revision": "4b677ebd90f6f54066365962d9e48dd0",
+    "url": "/static/media/BlackCatFacingLeft.4b677ebd.png"
+  },
+  {
+    "revision": "fa51ca47c0cbedd012ea3c00986545c8",
+    "url": "/static/media/BlackCatFacingRight.fa51ca47.png"
+  },
+  {
+    "revision": "daeafac7a3f3c7ec67125c627c6e39cb",
+    "url": "/static/media/GoldenCatFacingCenter.daeafac7.png"
+  },
+  {
+    "revision": "7c628c79a7be74c363b0ec251621320a",
+    "url": "/static/media/GoldenCatFacingLeft.7c628c79.png"
+  },
+  {
+    "revision": "b2a17de904139495ebdb92f59f63cd67",
+    "url": "/static/media/GoldenCatFacingRight.b2a17de9.png"
+  },
+  {
+    "revision": "da7ea278a221a2711593f3ec88315a74",
+    "url": "/static/media/GoldenSparklesCatFacingCenter.da7ea278.png"
+  },
+  {
+    "revision": "75c00a64014fae56396c766ed3e9f06b",
+    "url": "/static/media/GoldenSparklesCatStage1.75c00a64.png"
+  },
+  {
+    "revision": "7cab4a50ec10eda022013378a54ee2ae",
+    "url": "/static/media/GoldenSparklesCatStage2.7cab4a50.png"
+  },
+  {
+    "revision": "eec6b51d970fc29eba5735328056dfe2",
+    "url": "/static/media/LeftBlackArm.eec6b51d.png"
+  },
+  {
+    "revision": "402697077392ca7fe80c6c915505063b",
+    "url": "/static/media/LeftBlackArmExtended.40269707.png"
+  },
+  {
+    "revision": "ee2ee864b0da82b9163aa91ab203009f",
+    "url": "/static/media/LeftBlackArmExtendedExtra.ee2ee864.png"
+  },
+  {
+    "revision": "6aab69515fa82ad7bc90107ba6308964",
+    "url": "/static/media/LeftGoldenArm.6aab6951.png"
+  },
+  {
+    "revision": "aac60c4807a2b90b02a538e4617fe78a",
+    "url": "/static/media/LeftGoldenArmExtended.aac60c48.png"
+  },
+  {
+    "revision": "86bc52bc4ce64577a07494e4c7f3eafe",
+    "url": "/static/media/LeftGoldenArmExtendedExtra.86bc52bc.png"
+  },
+  {
+    "revision": "07e63b35ca95e7afd095387c0ce0d427",
+    "url": "/static/media/LeftOrangeArm.07e63b35.png"
+  },
+  {
+    "revision": "0e63dadefcd0f111eb5c8de9ab983cd6",
+    "url": "/static/media/LeftOrangeArmExtended.0e63dade.png"
+  },
+  {
+    "revision": "9ae5361db1c55fc88fe8b6d5d41d9a7d",
+    "url": "/static/media/LeftOrangeArmExtendedExtra.9ae5361d.png"
+  },
+  {
+    "revision": "69c49b559d641b1e1a253cd7a07e6303",
+    "url": "/static/media/OrangeCatFacingCenter.69c49b55.png"
+  },
+  {
+    "revision": "83d3d3825531625e796158c46770cd95",
+    "url": "/static/media/OrangeCatFacingLeft.83d3d382.png"
+  },
+  {
+    "revision": "7429849de7c5db3bd6ba1427d9a25167",
+    "url": "/static/media/OrangeCatFacingRight.7429849d.png"
+  },
+  {
+    "revision": "3b002fa36c1f1cc21060bc6f3176556d",
+    "url": "/static/media/RightBlackArm.3b002fa3.png"
+  },
+  {
+    "revision": "0847255151f62f9e40d9d6495a8f185c",
+    "url": "/static/media/RightBlackArmExtended.08472551.png"
+  },
+  {
+    "revision": "e8ec8ab03992e6e07138522f0a86e831",
+    "url": "/static/media/RightBlackArmExtendedExtra.e8ec8ab0.png"
+  },
+  {
+    "revision": "89e0c6123d4f0f4963e5623823a933f1",
+    "url": "/static/media/RightBlackArmExtendedExtraExtra.89e0c612.png"
+  },
+  {
+    "revision": "160651185ef7024781f21aafab62981c",
+    "url": "/static/media/RightGoldenArm.16065118.png"
+  },
+  {
+    "revision": "c8f8635773cec555e6627de5d798a5d6",
+    "url": "/static/media/RightGoldenArmExtended.c8f86357.png"
+  },
+  {
+    "revision": "3b9a057bf8533a4b73d90ab9281e4412",
+    "url": "/static/media/RightGoldenArmExtendedExtra.3b9a057b.png"
+  },
+  {
+    "revision": "8fd5055d037bf7d4c79941ae8d897033",
+    "url": "/static/media/RightGoldenArmExtendedExtraExtra.8fd5055d.png"
+  },
+  {
+    "revision": "9bbe28fcf9b19c46ff69724218ac3191",
+    "url": "/static/media/RightOrangeArm.9bbe28fc.png"
+  },
+  {
+    "revision": "dfa1b3cedd60231eee210446c8ed31a7",
+    "url": "/static/media/RightOrangeArmExtended.dfa1b3ce.png"
+  },
+  {
+    "revision": "4b7630c92d98cb9c7262dd004b57b69f",
+    "url": "/static/media/RightOrangeArmExtendedExtra.4b7630c9.png"
+  },
+  {
+    "revision": "d28d626b408566c75613a11f60edc2aa",
+    "url": "/static/media/RightOrangeArmExtendedExtraExtra.d28d626b.png"
+  },
+  {
+    "revision": "deac7243ad64fa9a3ec79adaf1b62f56",
+    "url": "/static/media/explosion.deac7243.mp3"
+  },
+  {
+    "revision": "e0d54496ceaa64d98cbc420ef64efa4b",
+    "url": "/static/media/keyboard.e0d54496.png"
+  },
+  {
+    "revision": "4c0f209b55d953b3a0f2fb3bcd31648e",
+    "url": "/static/media/laser.4c0f209b.mp3"
+  },
+  {
+    "revision": "bbdbba821d422937a074ecc920cf8eeb",
+    "url": "/static/media/meow.bbdbba82.mp3"
+  },
+  {
+    "revision": "a7c21348a70f24bd44d2fb5589308ed2",
+    "url": "/static/media/music.a7c21348.mp3"
+  },
+  {
+    "revision": "9b206c25486f1201798d6bcbb533da31",
+    "url": "/static/media/orange.9b206c25.png"
+  },
+  {
+    "revision": "b595b85f21ff16cfec8a5d8fbb2cffbe",
+    "url": "/static/media/purr.b595b85f.mp3"
+  },
+  {
+    "revision": "1ed60758898321662c8e43e87c6353a6",
+    "url": "/static/media/red1.1ed60758.png"
+  },
+  {
+    "revision": "0da345eeb2783f278e94f33311581556",
+    "url": "/static/media/red2.0da345ee.png"
+  },
+  {
+    "revision": "39096aee0f2ff45c8734c0b11ca85acd",
+    "url": "/static/media/typing.39096aee.mp3"
+  },
+  {
+    "revision": "bc8f8182a930fda9d2734b08f4598451",
+    "url": "/static/media/yellow.bc8f8182.png"
+  }
+]);
